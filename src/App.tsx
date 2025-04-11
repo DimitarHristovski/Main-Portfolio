de87d8dc2215d7b2d9bd1a5c69799f7e0de38f67
Dimitar <dimihbt@yahoo.com>
2025-04-11 18:45:58 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import LanguageSwitcher from "./components/LanguageSwitcher";
 import "./i18n/i18";
-import { ThemeProvider } from "./contexts/ThemeContext";
 import Hero from "./components/Hero";
 import { Skills } from "./components/Skills";
 import { Activity } from "./components/Activity";
@@ -9,6 +8,7 @@ import About from "./components/About";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
+import { ThemeProvider } from "./contexts/ThemeContext";
 
 function App() {
   return (
